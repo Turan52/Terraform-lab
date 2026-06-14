908c4fbd636a8683a9a0c4a79902061d7dec97fd
Ravan <105795532+Turan52@users.noreply.github.com>
2026-06-14 23:06:11 +0200

    Update file paths and content in main.tf

--- variables/main.tf
+++ variables/main.tf
@@ -8,12 +8,12 @@ terraform {
 
 resource "local_file" "hello" {
   content         = var.message
-  filename        = "/home/${var.username}/hello.txt"
+  filename        = "/home/filip/hello.txt"
   file_permission = "0644"
 }
 
 resource "local_file" "second" {
-  content         = "This is a second file created by Terraform"
-  filename        = "/home/${var.username}/second.txt"
+  content         = "THis is a second file created by Terraform"
+  filename        = "/home/filip/secondfile.txt"
   file_permission = "0644"
 }
